8c063a9be5cb80e5c4a8b13cec0310ce584af325
.classpathAsha-27-k <asha27k@gmail.com>
2024-11-10 12:31:24 +0530

    First commit

---
+++ src/main/java/com/pages/loginpage.java
@@ -0,0 +1,9 @@
+package com.pages;
+
+public class loginpage {
+	
+	public void login () {
+		System.out.println("This is first demo");
+	}
+
+}
---
+++ src/test/java/com/testcases/logintest.java
@@ -0,0 +1,9 @@
+package com.testcases;
+
+public class logintest {
+	
+	public void logintest() {
+		System.out.println("This is login test");
+	}
+
+}
